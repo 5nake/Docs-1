8f27941984b83f7c4e63aa15a87703efa34b39c4
Serafim <nesk@xakep.ru>
2014-08-05 13:38:50 +0400

    Add auth configs

--- app/views/page/auth.blade.php
+++ app/views/page/auth.blade.php
@@ -2,11 +2,15 @@
     @include('partial.header')
     <section class="auth container">
         <h2>Авторизация</h2>
+        @if (Config::get('auth.rudev'))
+        <a href="{{$authLink}}" class="button">Вход</a>
+        @else
         <form action="{{URL::route('login')}}" method="post">
             {{Form::token()}}
             <input type="text" name="login" placeholder="Логин" />
             <input type="password" name="password" placeholder="Пароль" />
             <input type="submit" value="Вход" />
         </form>
+        @endif
     </section>
 @stop
