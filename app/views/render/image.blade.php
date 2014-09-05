2fd9bc8d817ef0cb78ff3ac83028f43a3db37f15
Kirill Nesmeyanov <nesk@xakep.ru>
2014-09-05 21:42:34 +0400

    Update image.blade.php
    render fix

--- app/views/render/image.blade.php
+++ app/views/render/image.blade.php
@@ -1,12 +1,9 @@
 @section('content')
     <img src="{{URL::route('stream', $file->token)}}" style="
-        padding: 20px 0;
         width: {{$width}}px;
         height: {{$height}}px;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin: -{{$height/2 + 20}}px 0 0 -{{$width/2}}px;
+        margin: 20px auto;
+        display: block;
         box-shadow:
             0 0 0 1px rgba(0, 0, 90, .1),
             0 0 0 3px #fff,
